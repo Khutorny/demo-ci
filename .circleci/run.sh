44f1346566810a0c68a85d39f93566c525faeab3
v.khutorny <khutorny@gmail.com>
2018-12-09 14:31:34 +0200

    composer license added

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=7904684e3c8d017750c1f91ee638cd56ea8e475b
 
 curl --user $TOKEN: \
      --request POST \
-     --form revision=6901d970cce4c430c9b09a253c92af1c4e05285a \
+     --form revision=26ddd008abd27d0c9039959f9e2bf915fd034830 \
      --form config=@config.yml \
      --form notify=false \
      https://circleci.com/api/v1.1/project/github/khutorny/demo-ci/tree/master
