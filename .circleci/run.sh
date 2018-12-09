aa7352c52397624c84269da5c93fafc46482cd25
v.khutorny <khutorny@gmail.com>
2018-12-09 14:34:12 +0200

    security cron job added

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=7904684e3c8d017750c1f91ee638cd56ea8e475b
 
 curl --user $TOKEN: \
      --request POST \
-     --form revision=26ddd008abd27d0c9039959f9e2bf915fd034830 \
+     --form revision=44f1346566810a0c68a85d39f93566c525faeab3 \
      --form config=@config.yml \
      --form notify=false \
      https://circleci.com/api/v1.1/project/github/khutorny/demo-ci/tree/master
